1bf4d9fe675e1516c49dac88eab21bb970a1d1dd
Entao <entaoyang@163.com>
2025-05-14 11:54:31 +0800

    1.0.3

--- src/main/kotlin/io/github/yangentao/types/TypesUtils.kt
+++ src/main/kotlin/io/github/yangentao/types/TypesUtils.kt
@@ -1,5 +1,10 @@
 package io.github.yangentao.types
 
+val javaVersionInt: Int = System.getProperty("java.specification.version")?.toIntOrNull() ?: 0
+
+@Suppress("Since15")
+val Thread.tid: Long get() = if (javaVersionInt >= 19) this.threadId() else this.id
+
 @Suppress("UNCHECKED_CAST")
 internal inline fun <reified T> Collection<Any>.firstTyped(): T? {
     return this.firstOrNull { it is T } as? T
