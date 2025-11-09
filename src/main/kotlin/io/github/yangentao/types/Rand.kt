1bad839dd631e9dda03099a94f3e1c417208e1b0
Entao <entaoyang@163.com>
2025-11-09 15:25:49 +0800

    2.0.14

--- src/main/kotlin/io/github/yangentao/types/Rand.kt
+++ src/main/kotlin/io/github/yangentao/types/Rand.kt
@@ -7,32 +7,32 @@ import kotlin.random.nextInt
 import kotlin.random.nextLong
 
 object Rand {
-    val r = Random(System.currentTimeMillis())
+    val random = Random(System.currentTimeMillis())
     private const val MIN_ID: Long = 0x10000000
     private const val MAX_ID: Long = 0x7FFFFFFF
 
     fun nextID(): Long {
-        return r.nextLong(MIN_ID, MAX_ID)
+        return random.nextLong(MIN_ID, MAX_ID)
     }
 
     fun nextDouble(from: Double, until: Double): Double {
-        return r.nextDouble(from, until)
+        return random.nextDouble(from, until)
     }
 
     fun nextInt(range: IntRange): Int {
-        return r.nextInt(range)
+        return random.nextInt(range)
     }
 
     fun nextLong(range: LongRange): Long {
-        return r.nextLong(range)
+        return random.nextLong(range)
     }
 
     fun nextInt(from: Int, until: Int): Int {
-        return r.nextInt(from, until)
+        return random.nextInt(from, until)
     }
 
     fun nextLong(from: Long, until: Long): Long {
-        return r.nextLong(from, until)
+        return random.nextLong(from, until)
     }
 
     val code4: String get() = nextInt(1000, 9999).toString()
